dfe930e093ec76dda0531692ba63aae413905968
Cheng Jed <jed351@dyn3164-44.wlan.ic.ac.uk>
2023-01-26 21:24:59 +0100

    Revert "Update upload_to_hub.py"
    This reverts commit 3de2b7672091169d6cbfaf1d0c301db3a8f2a2c2.

--- upload_to_hub.py
+++ upload_to_hub.py
@@ -1,4 +1,4 @@
 from transformers import BertTokenizer
 
 tokenizer = BertTokenizer.from_pretrained('vocab-bart-base-cantonese.txt')
-tokenizer.push_to_hub('jed351/gpt2-tiny-zh-hk')
+tokenizer.push_to_hub('Ayaka/bart-base-cantonese')
